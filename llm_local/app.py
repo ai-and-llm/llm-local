92bca1fe0f3c94e6f98ed093b5292db501989ffe
Maksym Stepanenko <maksym.stepanenko@oracle.com>
2025-01-31 16:42:33 +0200

    LLama & Phi chats added

--- llm_local/app.py
+++ llm_local/app.py
@@ -5,24 +5,16 @@ import transformers
 from dotenv import load_dotenv
 from huggingface_hub import login
 
+
 def load_envs() -> None:
     load_dotenv()
     hugging_face_token = os.getenv('HUGGINGFACE_TOKEN')
     login(hugging_face_token)
 
-def main() -> None:
-    load_envs()
 
+def llama_chat(question: str) -> None:
     # https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct
     llm_model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-
-    # model = AutoModelForCausalLM.from_pretrained(
-    #     llm_model_name,
-    #     device_map="auto",
-    #     torch_dtype="auto",
-    #     trust_remote_code=True,
-    model_kwargs = {"torch_dtype": torch.bfloat16},
-    # )
 
     generator = transformers.pipeline(
         "text-generation",
@@ -31,51 +23,72 @@ def main() -> None:
         device_map="auto",
     )
 
-    text = "the Merge  button is not available for merge_requests/40 even taking into account that MR was properly approved. Can you merge mentioned MR?"
-
     messages = [
-        {"role": "system", "content": "You are an advanced AI language model designed to improve the readability of text while preserving its meaning. Your goal is to rewrite the given input to make it clearer, more engaging, and more natural. Ensure that the tone is appropriate for the intended audience, sentence structures are fluid, and complex or awkward phrasing is simplified without losing key details"},
-        {"role": "user", "content": f"Here is the text that needs improvement: ${text}"},
+        {"role": "system",
+         "content": "You are an advanced AI language model designed to improve the readability of text while preserving its meaning. Your goal is to rewrite the given input to make it clearer, more engaging, and more natural. Ensure that the tone is appropriate for the intended audience, sentence structures are fluid, and complex or awkward phrasing is simplified without losing key details"},
+        {"role": "user", "content": f"Here is the text that needs improvement: ${question}"},
     ]
+
+    # messages = [
+    #     {"role": "user", "content": f"Rewrite the following text to make it more human-readable, clear, and concise. Avoid jargon and complex sentences. Focus on making the message easy to understand for a general audience. Text: {question}"},
+    # ]
 
     outputs = generator(
         messages,
         max_new_tokens=256,
     )
-    print(outputs[0]["generated_text"][-1])
+    print(f"LLama response: ========> {outputs[0]['generated_text'][-1]['content']}")
 
-    # llm_model_name = "microsoft/Phi-3-mini-4k-instruct"
+
+def phi_chat(question: str) -> None:
     #
-    # # Load model and tokenizer
-    # model = AutoModelForCausalLM.from_pretrained(
-    #     llm_model_name,
-    #     device_map="auto",
-    #     torch_dtype="auto",
-    #     trust_remote_code=True,
-    # )
-    # tokenizer = AutoTokenizer.from_pretrained(llm_model_name)
+    # https://huggingface.co/microsoft/Phi-3-mini-4k-instruct
     #
-    # # Create a pipeline
-    # generator = transformers.pipeline(
-    #     "text-generation",
-    #     model=model,
-    #     tokenizer=tokenizer,
-    #     return_full_text=False, # Don't return prompt but only output text
-    #     max_new_tokens=500,
-    #     do_sample=False
-    # )
-    #
-    # text = "the Merge  button is not available for merge_requests/40 even taking into account that MR was properly approved. Can you merge mentioned MR?";
-    #
-    # # The prompt (user input / query)
+    llm_model_name = "microsoft/Phi-3-mini-4k-instruct"
+
+    # Load model and tokenizer
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        llm_model_name,
+        device_map="auto",
+        torch_dtype="auto",
+        trust_remote_code=True,
+    )
+    tokenizer = transformers.AutoTokenizer.from_pretrained(llm_model_name)
+
+    # Create a pipeline
+    generator = transformers.pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        return_full_text=False,  # Don't return prompt but only output text
+        max_new_tokens=500,
+        do_sample=False
+    )
+
+    # The prompt (user input / query)
     # messages = [
-    #     {"role": "user", "content": f"Rewrite the following text to make it more human-readable, clear, and concise. Avoid jargon and complex sentences. Focus on making the message easy to understand for a general audience. Text: {text}"},
+    #     {"role": "user", "content": f"Rewrite the following text to make it more human-readable, clear, and concise. Avoid jargon and complex sentences. Focus on making the message easy to understand for a general audience. Text: {question}"},
     # ]
-    #
-    # # Generate output
-    # output = generator(messages)
-    # generated_response = output[0]["generated_text"]
-    # print(generated_response)
+
+    messages = [
+        {"role": "system",
+         "content": "You are an advanced AI language model designed to improve the readability of text while preserving its meaning. Your goal is to rewrite the given input to make it clearer, more engaging, and more natural. Ensure that the tone is appropriate for the intended audience, sentence structures are fluid, and complex or awkward phrasing is simplified without losing key details"},
+        {"role": "user", "content": f"Here is the text that needs improvement: ${question}"},
+    ]
+
+    # Generate output
+    output = generator(messages)
+    print(f"Phi response: ========> {output[0]['generated_text']}")
+
+
+def main() -> None:
+    load_envs()
+
+    question = input("Make more human-readable: ")
+
+    llama_chat(question)
+
+    phi_chat(question)
 
 
 if __name__ == '__main__':
