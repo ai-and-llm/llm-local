4cc12245261065855109b3e223b8aa3b955b10ef
Maksym Stepanenko <maksym.stepanenko@oracle.com>
2025-02-05 17:27:19 +0200

    Code generified

--- llm_local/app.py
+++ llm_local/app.py
@@ -2,8 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from huggingface_hub import login
-from llm_local import llama, phi
-from llm_local.chat import Chat, Phi, Llama
+from llm_local.chat import Chat, Llama
 
 
 def load_envs() -> None:
@@ -12,34 +11,12 @@ def load_envs() -> None:
     login(hugging_face_token)
 
 
-def movie_review_sentiment_analysis(movie_review_feedback) -> None:
-    print("Do sentiment analysis for the movie review")
-
-    review_result = llama.classification(movie_review_feedback)
-
-    print(f"REVIEW: {movie_review_feedback}")
-    print(f"SENTIMENT ANALYSIS: {review_result}")
+# chat = Chat(Phi())
+chat = Chat(Llama())
 
 
-def enhance_sentence(sentence: str) -> None:
-    print("Make sentence more human readable")
-
-    # enhanced_sentence = llama.enhance_sentence(sentence)
-    enhanced_sentence = phi.enhance_sentence(sentence)
-
-    print(f"ORIGINAL: {sentence}")
-    print(f"ENHANCED: {enhanced_sentence}")
-
-
-def main() -> None:
-    load_envs()
-
-    # movie_review_sentiment_analysis(
-    #     "That was the most exciting horror movie that I've even seen."
-    # )
-    # enhance_sentence("Questions to think through: do you need the request to complete in 5 sec or is it OK for the request to take some time?")
-
-    chat = Chat(Phi())
+def chat_conversation() -> None:
+    print("Chat started")
 
     while True:
         question = input("Your question: ")
@@ -47,6 +24,41 @@ def main() -> None:
             break
         answer = chat.ask_question(question)
         print(f"Answer: {answer}")
+
+
+def enhance_sentences() -> None:
+    print("Make sentence more human readable")
+
+    while True:
+        sentence = input("Your sentence: ")
+        if sentence == "exit":
+            break
+        enhanced_sentence = chat.enhance_sentence(sentence)
+        print(f"ORIGINAL: {sentence}")
+        print(f"ENHANCED: {enhanced_sentence}")
+
+
+def movie_review_sentiment_analysis() -> None:
+    print("Do sentiment analysis for the movie review")
+
+    while True:
+        movie_review_feedback = input("Write movie review: ")
+        if movie_review_feedback == "exit":
+            break
+
+        review_result = chat.classify_movie_review(movie_review_feedback)
+
+        print(f"SENTIMENT ANALYSIS: {review_result}")
+
+
+def main() -> None:
+    load_envs()
+
+    # chat_conversation()
+
+    # enhance_sentences()
+
+    movie_review_sentiment_analysis()
 
 
 if __name__ == "__main__":
