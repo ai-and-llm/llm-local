2db0f45e790dee2e303c464393077f2ead0494e1
Maksym Stepanenko <maksym.stepanenko@oracle.com>
2025-02-05 15:35:42 +0200

    Chat with Phi fully completed

--- llm_local/chat.py
+++ llm_local/chat.py
@@ -1,40 +1,31 @@
-# Define an abstract base class
-
-import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 
 class Chat:
-
     def __init__(self):
         # https://huggingface.co/microsoft/Phi-3-mini-4k-instruct
         llm_model_name = "microsoft/Phi-3-mini-4k-instruct"
 
         print(f"Loading '{llm_model_name}' LLM model")
-        self.model = transformers.AutoModelForCausalLM.from_pretrained(
-            llm_model_name,
-            device_map="auto",
-            torch_dtype="auto",
-            trust_remote_code=True,
-        )
+        self.model = AutoModelForCausalLM.from_pretrained(llm_model_name)
 
         print(f"Loading '{llm_model_name}' tokenizer")
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(llm_model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(llm_model_name)
 
     def ask_question(self, question: str) -> str:
-
         # Create a pipeline
-        generator = transformers.pipeline(
+        generator = pipeline(
             "text-generation",
             model=self.model,
             tokenizer=self.tokenizer,
-            return_full_text=False,  # Don't return prompt but only output text
             max_new_tokens=500,
+            return_full_text=False,  # Don't return prompt but only output text
             do_sample=False,
         )
 
         prompt = [
             {"role": "system", "content": "You are a helpful AI assistant."},
-            {"role": "user", f"content": f"{question}"},
+            {"role": "user", "content": f"{question}"},
         ]
 
         # Generate output
@@ -42,4 +33,3 @@ class Chat:
         enhanced_sentence = output[0]["generated_text"]
 
         return enhanced_sentence
-
